57a4070aa94a83f65bdd66555a315499b5820737
Lívia Rocha <liviaantenucci@colegiopaulodetarso.com.br>
2025-10-29 15:04:48 +0000

    loops2

--- loops2.py
+++ loops2.py
@@ -1,9 +1,9 @@
 inventario = ["elmo", "chapéu", "espada", "botas"]
-busca = input("Digite um item: ")
+busca = input("Busque por um item: ")
 
 for item in inventario:
     if item == busca:
-        print(f"{busca.capitalize()} foi encontrada!")
+        print(f"{busca.capitalize()} foi encontrado(a)!")
         break
 else:
     print("Item não encontrado.")    
