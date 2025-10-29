26f747a38ea10e41a31484c86da39fe5dbf41a30
Lívia Rocha <liviaantenucci@colegiopaulodetarso.com.br>
2025-10-29 15:32:42 +0000

    loops3

--- loops3.py
+++ loops3.py
@@ -1,4 +1,4 @@
-usuarios = ["pietroraibow@", "lostqqz", "zard", "", "guigo", "progold"]
+usuarios = ["pietroraibow", "lostqqz", "zard", "", "guigo", "progold"]
 
 for usuario in usuarios:
     if usuario:
